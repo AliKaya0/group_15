8fe69b6477c82487f6e5a32c15ddcb517548abb2
aLihy <ali.hydr.kaya@hotmail.com>
2022-02-20 22:40:09 -0500

    Initial commit

---
+++ src/interview_question/String_Frequency_Characters.java
@@ -0,0 +1,14 @@
+package interview_question;
+
+public class String_Frequency_Characters {
+
+    public static void main(String[] args) {
+        System.out.println("run");
+    }
+
+}
+/*
+1)  String - Frequency of Characters
+Write a return method that can find the frequency of characters
+Ex:  FrequencyOfChars("AAABBCDD") ==> A3B2C1D2
+ */
---
+++ src/interview_question/String_Same_Letters.java
@@ -0,0 +1,10 @@
+package interview_question;
+
+public class String_Same_Letters {
+}
+/*
+2) String - Same letters
+Write a return method that check if a string is build out of the same letters as another string.
+Ex:  same("abc",  "cab"); -> true
+same("abc",  "abb"); -> false:
+ */
